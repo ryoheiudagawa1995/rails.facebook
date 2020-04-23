69ef34028f895b284fc37f02d39a56f7b76f2c0c
ryohei udagawa <r.udagawa1995@gmail.com>
2020-04-23 18:16:07 +0900

    投稿確認

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,28 +1,15 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
-
-  # GET /blogs
-  # GET /blogs.json
   def index
     @blogs = Blog.all
   end
-
-  # GET /blogs/1
-  # GET /blogs/1.json
   def show
   end
-
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
-
-  # GET /blogs/1/edit
   def edit
   end
-
-  # POST /blogs
-  # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
     @blog.user_id = current_user.id
@@ -49,9 +36,6 @@ class BlogsController < ApplicationController
       end
     end
   end
-
-  # DELETE /blogs/1
-  # DELETE /blogs/1.json
   def destroy
     @blog.destroy
     respond_to do |format|
@@ -59,15 +43,14 @@ class BlogsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  def confirm
+    @blog = Blog.new(blog_params)
+  end
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def blog_params
-      params.require(:blog).permit(:title, :content, :image, :image_cache)
-    end
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+  def blog_params
+    params.require(:blog).permit(:title, :content, :image, :image_cache)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  resources :blogs
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
